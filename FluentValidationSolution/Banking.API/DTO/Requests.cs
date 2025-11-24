5e2a5a8eccf86f763eccfd9810684872f29d17a0
Rafael Bonotto <rafareisaba@outlook.com>
2025-11-24 20:53:29 -0300

    Adiciona Requests

---
+++ FluentValidationSolution/Banking.API/DTO/Requests.cs
@@ -0,0 +1,27 @@
+﻿using Banking.API.Models;
+
+namespace Banking.API.DTO
+{
+    public class Requests
+    {
+         public record CriarCOntaRequest(string NomeTitular, string CPF, TipoConta Tipo);
+
+        public record DepositoRequest(string NumeroConta, decimal Valor)
+        {
+            // Construtor para permitir with expression
+            public DepositoRequest() : this("", 0) { }
+        }
+
+        public record SaqueReques(string NumeroConta, decimal Valor)
+        {
+            public SaqueReques() : this("", 0) { }
+        }
+        
+        public record TransferenciaReques(
+            string ContaOrigem,
+            string ContaDestino,
+            decimal Valor,
+            string? Descricao = null
+        );
+    }
+}
